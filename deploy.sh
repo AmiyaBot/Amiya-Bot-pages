b296564d8f406b9a5cf80161e299e0fd0f80e715
vivien <826197021@qq.com>
2021-12-11 20:15:26 +0800

    update push

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:AmiyaBot/Amiya-Bot.git master:gh-pages
+git push -f git@github.com:AmiyaBot/Amiya-Bot-pages.git master:gh-pages
 
 cd -
