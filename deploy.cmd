e8533f9c82a2967dc8edf59c4c981e587f767f9a
vivien <826197021@qq.com>
2021-12-23 19:41:25 +0800

    delete deploy script

--- deploy.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo. Pushing...
-git init>nul
-git add -A>nul
-git commit -m 'deploy'>nul
-git push -f git@github.com:AmiyaBot/Amiya-Bot-pages.git master:main>nul
-cls
-echo. Push complete!
-pause
